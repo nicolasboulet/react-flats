067dd25a365ecf5981bde69ffce1a303bf85dec7
Nicolas Boulet <nicolasboulet@live.fr>
2021-09-19 11:10:57 +0200

    a finir plus tard

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -4,7 +4,6 @@ import '../../assets/stylesheets/cards.scss';
 
 const Flat = ({ name, price, pricecurrency, imageUrl }) => {
   return (
-    
     <div className="card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${ imageUrl }')` }}>
       <div className="card-category">
         { `${price} `}
@@ -13,7 +12,7 @@ const Flat = ({ name, price, pricecurrency, imageUrl }) => {
       <div className="card-description">
         <h2>{name}</h2>
       </div>
-      <a className="card-link" href="#"></a>
+      <a className="card-link" href="#a">clic</a>
     </div>
   );
 };
